7569b734bb108fe84021d0acee9cf097f7e1e450
moshengwei <moshengwei@krspace.cn>
2017-05-11 16:47:40 +0800

    优化点对点通讯

--- src/main/java/com/moven/websockets/WebSocketTestChat.java
+++ src/main/java/com/moven/websockets/WebSocketTestChat.java
@@ -1,10 +1,9 @@
 package com.moven.websockets;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
-import javax.servlet.http.HttpSession;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
@@ -15,8 +14,18 @@ import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/chat/{username}/{receivername}")
 public class WebSocketTestChat {
-	Set<Session> session_list = null;
+	//广播地址列表
+//	Set<Session> session_list = null;
+	//用户连接池
+	static final Map<String, Session> sessionPool = new HashMap<String, Session>();
 
+	/**
+	 * Description:广播式方法
+	 * @author moshengwei
+	 * @date 2017年5月11日 下午4:46:09
+	 * @param message	发送的消息
+	 * @param session	连接的用户
+	 */
 //	@OnMessage
 //	public void onMessage(String message, Session session) {
 //		try{
@@ -40,20 +49,25 @@ public class WebSocketTestChat {
 //		}
 //	}
 	
+	/**
+	 * Description:点对点发送
+	 * @author moshengwei
+	 * @date 2017年5月11日 下午4:46:42
+	 * @param message	发送的消息
+	 * @param session	连接的用户
+	 */
 	@OnMessage
 	public void onMessage(String message, Session session) {
 		try{
-			session_list = session.getOpenSessions();
 			Map<String, String> params = session.getPathParameters();
 			String username = params.get("username");
 			String receivername = params.get("receivername");
-			for (Session s : session_list) {
-				Map<String, String> tempParams = s.getPathParameters();
-				String t_username = tempParams.get("username");
-				String t_receivername = tempParams.get("receivername");
-				if(receivername.equals(t_username) || receivername.equals(t_receivername)){
-					s.getBasicRemote().sendText("["+username + "] 对 [" + receivername + "] 说:" + message);
-				}
+			Session receiver = sessionPool.get(receivername);
+			if(null != receiver){
+				receiver.getBasicRemote().sendText("["+username + "] 对 [" + receivername + "] 说:" + message);
+				session.getBasicRemote().sendText("["+username + "] 对 [" + receivername + "] 说:" + message);
+			} else {
+				session.getBasicRemote().sendText("["+receivername+"] 未连接到服务器");
 			}
 		} catch(IOException e){
 			System.out.println("Client error");
@@ -67,17 +81,18 @@ public class WebSocketTestChat {
 	
 	@OnOpen
 	public void onOpen(@PathParam("username") String username, Session session) {
-		HttpSession httpSession = null;
-		if(session instanceof HttpSession){
-			httpSession = (HttpSession)session;
-			httpSession.setAttribute("username", username);
+		if(null != session){
+			sessionPool.put(username, session);
 		}
-		System.out.println("Client connected");
+		System.out.println("Client connected! username : "+username);
 	}
 
 	@OnClose
-	public void onClose() {
-		System.out.println("Connection closed");
+	public void onClose(@PathParam("username") String username, Session session) {
+		if(null != session){
+			sessionPool.remove(username);
+		}
+		System.out.println("Connection closed! username : "+username);
 	}
 
 }
